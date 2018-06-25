39e7af0db3cb9d544223f60f60ad92109dbaf3f9
yseult1989 <40201909+yseult1989@users.noreply.github.com>
2018-06-25 19:23:30 +0200

    Create reverse_string.php

---
+++ reverse_string.php
@@ -0,0 +1,13 @@
+<?php
+
+$str = 'abcde';
+
+
+function reverseStr($str){
+	$tab=[];
+	for($i = 0; $i <= isset($str[$i]); $i++){
+		$tab[] =$str[$i];
+	}
+	return $sReverse =  implode('', array_reverse($tab));
+}
+
